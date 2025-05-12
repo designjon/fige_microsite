a4ff16dcc2eefe32dd7a64fe79a1003b41841aea
Jon Friedman <jonfriedman@Mac.lan>
2025-05-11 21:35:39 -0500

    Fix API date for Stripe

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -2,7 +2,7 @@ import { NextRequest } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2022-11-15",
 });
 
 export async function POST(req: NextRequest) {
