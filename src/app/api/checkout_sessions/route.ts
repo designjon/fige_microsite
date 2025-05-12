4d8d8b83fb847b560883500b498d9105a3037b39
Jon Friedman <jonfriedman@Mac.lan>
2025-05-11 21:39:47 -0500

    Fix type error on unitId from request body

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export async function POST(req: NextRequest) {
-  const { unitId } = await req.json();
+  const { unitId }: { unitId: string } = await req.json();
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
